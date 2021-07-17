bcfa2ddd0c17082aeb26b781b5708bac58bdc27d
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-07-17 23:17:50 +0600

    php output declare

---
+++ output.php
@@ -0,0 +1,14 @@
+<?php
+// most importatant printf(); method
+$fname="Shamim";
+$lname= "ahmed";
+
+echo "My full name is ".$fname." ".$lname."\n";
+printf("My %s name is %s %s",'full', $fname, $lname."\n");
+
+// printf method use output
+
+$plannet1 = "Murcuy";
+$plannet2 ="Jupiter";
+
+printf("The smallest is %s and the biggest is %s", $plannet1, $plannet2 );
