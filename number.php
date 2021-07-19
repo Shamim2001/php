6517e0cf69181adcc1e07bff208fc14c9eadcdff
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-07-19 15:54:40 +0600

    deffent number print

---
+++ number.php
@@ -0,0 +1,14 @@
+<?php
+$n = 12;
+$Oct = 017;
+$hexa = 0x1B;
+
+// printf("The number is %d and %d", $n, $Oct);
+
+// printf("the binary equivalent of %d is %b", 1212, 1212);
+printf("the hexadecimal equivalent of %d = %X \n", 1212, 1212);
+printf("the octal equivalent of %d = %o \n",27, 27);
+
+printf("the octal equivalent of %d = %o \n", 65536, 65536);
+
+printf("the ocatal %o = %d", 0200000, 0200000);
--- oparetor.php
+++ oparetor.php
@@ -1,10 +1,34 @@
 <?php
 // =, -, *, /, += arithemetic oparetor
-$num = 12;
+// $num = 12;
 
 // $num += 13;
 // $num -= 13;
 // $num *= 13;
-$num /= 13;
+// $num /= 13;
 
-echo $num;
+// echo $num;
+
+// assinment oparetor
+
+$n = 7;
+
+// $m = $n ++;
+// echo $m, "\n", $n;
+
+$m = ++$n;
+echo $m, "\n", $n;
+
+/*
+
+$m = $n++;
+$m = $n ;
+$m = $n + 1;
+
+*/
+
+/*
+ $m = ++$n;
+$m = $n + 1 ;
+$m = $n;
+*/
