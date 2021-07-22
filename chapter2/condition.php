beb41cca1c663af106b49e5416c9f63f487347e5
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-07-22 20:24:05 +0600

    conditon check

---
+++ chapter2/condition.php
@@ -0,0 +1,17 @@
+<?php
+
+// $n = 10;
+
+// if($n % 2==0 ) {
+//     echo ' ($n) is even number';
+// } else {
+//     echo '{$n} odd number';
+// }
+
+$food = 'apple';
+
+if ($food = "apple") {
+    echo "apple is Vitamin E";
+} else {
+    echo "apple is more VItamin";
+}
