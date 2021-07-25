77f4d8133ba507cf5adc9100fe37987b579d0b29
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-07-25 23:57:13 +0600

    nested switch condition

---
+++ chapter2/nestedswitch.php
@@ -0,0 +1,42 @@
+<?php
+
+$n = 14;
+$r = $n %2;
+/*
+switch ($r) {
+    case 0:
+        switch ($n) {
+            case $n > 0:
+                echo "$n is a positive even number";
+                break;
+            case $n < 0:
+                echo "$n is a negetive even number";
+                break;
+        }
+        break;
+    default:
+        switch ($n) {
+            case $n > 0:
+                echo "$n is a positive odd number";
+                break;
+            case $n < 0:
+                echo "$n is a negetive odd number";
+                break;
+        }
+} */
+echo "\n";
+
+switch (true) {
+    case ($r == 0 && $n > 0):
+        echo "$n is a positive even number";
+        break;
+    case ($r == 1 && $n > 0):
+        echo "$n is a negetive odd number";
+        break;
+    case ($r == 0 && $n < 0):
+        echo "$n is a positive odd number";
+        break;
+    case ($r == 1 && $n < 0):
+        echo "$n is a negetive even number";
+        break;
+}
