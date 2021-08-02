c78c7fde1b5a8ec500355b099b0a31a4ae0fc8fd
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-08-02 10:37:56 +0600

    function parameters

--- chapter4/function.php
+++ chapter4/function.php
@@ -1,10 +1,18 @@
 <?php
 include_once "isFunction.php";
 
-$n = 12;
-
+$n = "4";
+/*
+// even/ odd number check
 if (isEven($n)) {
     echo "{$n} is an Even number";
 } else {
     echo "{$n} is an Odd number";
 }
+
+//factorial number call
+echo "Factorial of {$n} is ".factorial($n);
+*/
+
+// food served function call
+serve('Coffie', '5 Plates');
--- chapter4/isFunction.php
+++ chapter4/isFunction.php
@@ -1,8 +1,22 @@
 <?php
-
+// evenOrOdd number function
 function isEven($n) {
     if ($n % 2 == 0) {
         return true;
     }
     return false;
+}
+
+// factorial number function
+function factorial(int $n) {
+    $result = 1;
+    for ($i = $n; $i >1; $i--) {
+        $result *= $i;
+    }
+    return $result;
+}
+
+// food served function 
+function serve($foodtype= "Coffie", $numberOfItem= "2 Plates") {
+    echo "{$numberOfItem} of {$foodtype} has/have been served";
 }
