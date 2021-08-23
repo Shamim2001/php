6ec78d0c9c6db3b7cd916f04155ccd855c9d1c01
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-08-23 23:03:01 +0600

    part by past string

---
+++ partstring.php
@@ -0,0 +1,26 @@
+<?php
+
+// part by part string 
+$string = "hello world, How are you";
+
+$parts = explode(" ", $string);
+print_r($parts);
+
+echo PHP_EOL;
+
+$original = join(" ", $parts);
+echo $original;
+
+echo PHP_EOL;
+
+// $parts2 = str_split($string);
+// print_r($parts2); // string token output
+
+echo PHP_EOL;
+
+$parts3 = strtok($string, " ");
+while ($parts3 !== false) {
+    echo $parts3;
+    echo PHP_EOL;
+    $parts3 = strtok(" ");
+}
