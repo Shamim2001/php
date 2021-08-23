6672788ec9ba8b45464abec93a5c8ad230e62c61
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-08-23 22:29:19 +0600

    string reverse

--- string.php
+++ string.php
@@ -2,4 +2,20 @@
 $string = "hello world";
 // echo $string[0];
 // echo $string[-1];
-echo substr($string, -5);
+// echo substr($string, -5);
+
+// string reverse
+$length = strlen($string)-1;
+
+for ($i =$length; $i >=0; $i--) {
+    echo $string[$i];
+}
+echo PHP_EOL;
+$length = strlen($string);
+for ($i = 1; $i <= $length; $i++) {
+    echo $string[$i * -1];
+}
+
+echo PHP_EOL;
+
+echo strrev($string);
