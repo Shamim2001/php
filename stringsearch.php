026335063df73c6362236281f6384c566d1a013f
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-08-28 23:15:04 +0600

    string search string

---
+++ stringsearch.php
@@ -0,0 +1,14 @@
+<?php
+
+$string = "My name is Shamim Ahmed";
+
+// echo $string[11];
+
+// echo strpos($string, "Shamim \n"); // srtpos() string search keyword
+
+// echo stripos($string, "shamim"); // srtipos() string search incase sensitive keyword
+
+// echo strripos($string, "shamim"); // strripos() string search last value incase sensitive  keyword
+
+echo strrpos($string, "d"); // strrpos() string search last value keyword
+
