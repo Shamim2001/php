1b53fba82401a1927c8edb794a2d94af71a11770
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-08-28 23:43:48 +0600

    string search string

--- stringsearch.php
+++ stringsearch.php
@@ -11,4 +11,13 @@ $string = "My name is Shamim Ahmed";
 // echo strripos($string, "shamim"); // strripos() string search last value incase sensitive  keyword
 
 echo strrpos($string, "d"); // strrpos() string search last value keyword
+echo PHP_EOL;
 
+
+// =========string search string=========
+
+$string = "My name is Samim SHAMIM Ahmed";
+
+$replacedString = str_replace('Samim', 'Shamim', $string); // Samim replace Shamim
+
+echo $replacedString;
