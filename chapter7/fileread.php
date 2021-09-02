65a65b9badb480b3d83898c6f837cb3b5ab1b650
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-09-02 23:13:12 +0600

    read data file

--- chapter7/fileread.php
+++ chapter7/fileread.php
@@ -6,4 +6,18 @@ $fp = fopen($filename, 'r');
 while ($line = fgetc($fp)) {
     echo $line;
 }
+rewind($fp);
+fseek($fp, 2);
+
+while ($line = fgetc($fp)) {
+    echo $line;
+}
 fclose($fp);
+
+// quickly read data
+$data = file($filename);
+print_r($data);
+
+// second method quickly read data 
+$readData = file_get_contents($filename);
+echo $readData;
