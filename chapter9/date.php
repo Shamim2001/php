581f60527288cf8a73a99acc169bfa239ef91600
shamim2001 <shamimahmedridoy2016@gmail.com>
2021-09-16 08:31:34 +0600

    date function datails

---
+++ chapter9/date.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set('Asia/Dhaka'); // bangladesh time zone
+echo date('d/F/Y');
+echo "\n"; // 16/September/2021
+echo date('dS F, y h:i:s A');  // 16th Septermber, 2021 time: 8:09:57 AM
+echo "\n";
+echo date('t'); // month count day
